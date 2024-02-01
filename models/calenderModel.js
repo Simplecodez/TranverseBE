b06e6b1754f4261f439382a7bfee482c5335feec
Emmanuel Elendu <emmanuel.elendu.197802@unn.edu.ng>
2024-02-01 10:10:30 +0100

    Project creation features

--- models/calenderModel.js
+++ models/calenderModel.js
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import validator from "validator";
+import mongoose from 'mongoose';
+import validator from 'validator';
 
-const userSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    required: [true, "Please provide a project name."],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  time: {
-    type: Number,
-    default: 1,
-  },
-});
+const calenderSchema = new mongoose.Schema({
+    description:{
+        type: String,
+        required: [true, 'Please provide a project name.']
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+    time:{
+        type: Number,
+        default: 1
+    },
+})
--- models/projectModel.js
+++ models/projectModel.js
@@ -1,44 +1,37 @@
-import mongoose from "mongoose";
-import validator from "validator";
+import mongoose from 'mongoose';
+import validator from 'validator';
 
 const projectSchema = new mongoose.Schema({
-  projectName: {
-    type: String,
-    required: [true, "Please provide a project name."],
-  },
-  description: {
-    type: String,
-    required: [true, "Please provide a project description."],
-  },
-  createdBy: {
-    type: mongoose.Schema.ObjectId,
-    ref: "User",
-  },
-  teamMembers: {
-    type: Array,
-    default: [],
-  },
-  duration: {
-    type: Number,
-    required: [true, "Please provide a duration."],
-  },
-  status: {
-    type: String,
-    enum: ["to-do", "in-progress", "completed"],
-    default: "to-do",
-  },
-  startDate: {
-    type: Date,
-    required: [true, "Please provide a start date."],
-  },
-  endDate: {
-    type: Date,
-    required: [true, "Please provide an end date."],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+    projectName:{
+        type: String,
+        required: [true, 'Please provide a project name.']
+    },
+    Description:{
+        type: String,
+        required: [true, 'Please provide a project description.']
+    },
+    teamMembers:{
+        type: Array,
+        default: []
+    },
+    duration:{
+        type: Number,
+        required:[true, "Please provide a duration."],
+        default: 1
+    },
+    status:{
+        type: String,
+        enum: ["to-do", "in-progress", "completed"],
+        default: "to-do"
+    },
+    startDate:{
+        type: Date,
+        required:[true, "Please provide a start date."]
+    },
+    endDate:{
+        type: Date,
+        required:[true, "Please provide an end date."]
+    }
+})
 
-export default mongoose.model("Project", projectSchema);
+export default mongoose.model('Project', projectSchemaSchema);
