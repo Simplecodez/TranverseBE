4cbb5f6c903dae77a19213b29bfcf9ae1358c164
Primesoft360 <elenduemmanuelelano@gmail.com>
2024-02-10 11:32:53 +0100

    Updated fields required to create a project

--- models/projectModel.js
+++ models/projectModel.js
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose';
-import validator from 'validator';
+import mongoose from "mongoose";
+import validator from "validator";
 
 const projectSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: [true, 'Please provide a project name.'],
+    required: [true, "Please provide a project name."],
     validate: {
       validator: (value) => {
         return (
@@ -12,12 +12,12 @@ const projectSchema = new mongoose.Schema({
           validator.matches(value, /^[a-zA-Z\s]+$/)
         );
       },
-      message: 'Please provide a project title.'
-    }
+      message: "Please provide a project title.",
+    },
   },
   description: {
     type: String,
-    required: [true, 'Please provide a project description.'],
+    required: [true, "Please provide a project description."],
     validate: {
       validator: (value) => {
         return (
@@ -25,85 +25,85 @@ const projectSchema = new mongoose.Schema({
           validator.matches(value, /^[a-zA-Z\s.]+$/)
         );
       },
-      message: 'Please provide a project description.'
-    }
+      message: "Please provide a project description.",
+    },
   },
   owner: {
     type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: true
+    ref: "User",
+    required: true,
   },
   teamMembers: {
     type: [
       {
         user: {
           type: mongoose.Schema.ObjectId,
-          ref: 'User',
-          required: true
+          ref: "User",
+          required: true,
         },
         role: {
           type: String,
-          enum: ['team-lead', 'member'],
-          default: 'member',
-          required: true
-        }
-      }
+          enum: ["team-lead", "member"],
+          default: "member",
+          required: true,
+        },
+      },
     ],
-    default: []
+    default: [],
   },
   tasks: {
     type: [
       {
         title: {
           type: String,
-          required: true
+          required: true,
         },
         description: {
-          type: String
+          type: String,
         },
         assignedTo: {
           type: mongoose.Schema.ObjectId,
-          ref: 'User'
+          ref: "User",
         },
         status: {
           type: String,
-          enum: ['Todo', 'In Progress', 'Done'],
-          default: 'Todo'
-        }
-      }
+          enum: ["Todo", "In Progress", "Done"],
+          default: "Todo",
+        },
+      },
     ],
-    default: []
+    default: [],
   },
   price: {
     type: Number,
     default: 10,
-    required: [true, 'Please provide the price for an hour.']
+    required: [true, "Please provide the price for an hour."],
   },
   duration: {
     type: Number,
-    required: [true, 'Please provide a duration.'],
-    default: 1
+    required: [true, "Please provide a duration."],
+    default: 1,
   },
   status: {
     type: String,
-    enum: ['to-do', 'in-progress', 'completed'],
-    default: 'to-do'
+    enum: ["to-do", "in-progress", "completed"],
+    default: "to-do",
   },
   startDate: {
     type: Date,
-    required: [true, 'Please provide a start date.']
+    required: [true, "Please provide a start date."],
   },
   endDate: {
     type: Date,
-    required: [true, 'Please provide an end date.']
+    required: [true, "Please provide an end date."],
   },
   active: {
     type: Boolean,
     default: true,
-    select: false
-  }
+    select: false,
+  },
 });
 
-const Project = mongoose.model('Project', projectSchema);
+const Project = mongoose.model("Project", projectSchema);
 
 export default Project;
