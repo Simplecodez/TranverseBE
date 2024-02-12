3d9afe6654dad2f83c36bc4ed77fe827ee7d216f
Emmanuel Elendu <emmanuel.elendu.197802@unn.edu.ng>
2024-02-13 00:33:23 +0100

    Updated user reset password route

--- controllers/authController.js
+++ controllers/authController.js
@@ -89,7 +89,13 @@ const restrictTo = (...roles) => {
 
 const protect = catchAsync(async (req, res, next) => {
   // get token and check if it exist
-  const token = req.cookies.jwt;
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
   if (!token) {
     return next(
       new AppError('Your are not logged in! Please log in to get access.', 401)
