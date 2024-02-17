8cb77784cdf14de379353e04942a8c8818e0e244
Emmanuel Elendu <emmanuel.elendu.197802@unn.edu.ng>
2024-02-17 06:32:29 +0100

    Fixed assigning task bug

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -52,8 +52,10 @@ const updateProjectStatus = catchAsync(async (req, res, next) => {
   const { status } = req.body;
   const project = await Project.findOne({ _id: req.params.id });
   if (!project) return next(new AppError('Sorry, this project does not exist!', 400));
-  if (project && project.owner !== req.user._id)
+  console.log(project, project.owner, req.user._id);
+  if (project && !project.owner.equals(req.user._id))
     return next(new AppError('Your are not authorized for this action!', 401));
+
   project.status = status;
   await project.save();
 
